5b3e74bcf89bc7ecfbe4adf39d29f7da9466d3c7
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2020-10-29 00:18:48 -0400

    updated

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 
-var colors = ["#00E5FF", "#00E676", "#FFAB40", "#2196F3", "#651FFF", "#EA80FC", "#E040FB", "#5D4037", "#FF5722"];
+var colors = ["#00E5FF", "#00E676", "#FFAB40", "#2196F3", "#651FFF", "#EA80FC", "#E040FB", "#FF5722", "#BCAAA4"];
 
 $(function() {
     let i = Math.floor(Math.random() * Math.floor(colors.length));
